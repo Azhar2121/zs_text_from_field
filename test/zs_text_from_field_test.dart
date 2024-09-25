a07cf89988510d8801f82bfd31abdd1431165279
Azhar2121 <azharuddin@excellisit.net>
2024-09-26 03:30:56 +0530

    Initial commit

---
+++ test/zs_text_from_field_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zs_text_fromfield/zs_text_from_field.dart';
+
+void main() {
+  test('adds one to input values', () {
+    CustomTextField();
+  });
+}
